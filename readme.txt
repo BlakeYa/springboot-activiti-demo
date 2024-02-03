cc531e9ffeb1eb2002b6039bfe4bead053a0cfe6
BlakeYa <blakeya@126.com>
2024-02-04 01:31:51 +0800

    test

--- readme.txt
+++ readme.txt
@@ -10,3 +10,8 @@
 7、mybatis generator自动生成代码插件
 8、日志使用logback
 9、整合swagger
+
+注意：
+1. 数据库mysql5.7
+2. 连续配置要把useSSL=false 改成false，默认好像也是true，搞了好长时间。恶心的很。
+3. datasource.properties 中的driver可以至直接制定maven仓库中的jar包。
--- src/main/java/com/neimeng/workflow/controller/ModelerController.java
+++ src/main/java/com/neimeng/workflow/controller/ModelerController.java
@@ -15,10 +15,12 @@ import org.activiti.engine.runtime.ProcessInstance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.jdbc.JdbcRepositoriesAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletResponse;
